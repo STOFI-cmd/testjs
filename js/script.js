9432ad86d3fca71f9059f403473ea7c23bfc2109
Amiran <protonguy9@gmail.com>
2022-06-15 16:49:19 +0400

    Third commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
 "use strict";
-const numberOfFilms = prompt("Сколько вы фильмов посмотрели?"),
-  personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-  };
-const a = prompt("Один из последних фильмов?"),
-  b = prompt("Как его оцените?"),
-  c = prompt("Один из последних фильмов?"),
-  d = prompt("Как его оцените?");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB.count);
-console.log(personalMovieDB.movies);
+for (let i = 1; i < 8; i++) {
+  console.log(i);
+  for (let j = 1; i < 8; j++) {
+    console.log(i);
+  }
+}
