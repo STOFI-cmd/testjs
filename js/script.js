3454bae5a792a701e923fd4208a245c5eff79b98
Amiran <protonguy9@gmail.com>
2022-06-19 19:53:46 +0400

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
 "use strict";
-for (let i = 1; i < 8; i++) {
-  console.log(i);
-  for (let j = 1; i < 8; j++) {
-    console.log(i);
+const numberOfFilms = prompt("Сколько вы фильмов посмотрели?"),
+  personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+  };
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log("Вы средний зритель");
+} else if (personalMovieDB.count > 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних фильмов?"),
+    b = prompt("Как его оцените?");
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    console.log("Проверка пройдена");
+    personalMovieDB.movies[a] = b;
+  } else {
+    console.log("error");
+    i--;
   }
 }
+
+//
+//
+
+console.log(personalMovieDB.count);
+console.log(personalMovieDB.movies);
+console.log("done");
